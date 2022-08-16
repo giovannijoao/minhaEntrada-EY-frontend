a912daf81126824ba9066f123f0e11818e38e9c5
João Giovanni <joaogiovannivilela@gmail.com>
2022-08-16 20:45:32 -0300

    Added authorization

--- services/cmsClient.ts
+++ services/cmsClient.ts
@@ -1,7 +1,10 @@
 import axios from "axios"
 const cmsClient = axios.create({
   baseURL:
-    process.env.CMS_API_URL || "http://127.0.0.1:1337/api"
+    process.env.CMS_API_URL || "http://127.0.0.1:1337/api",
+  headers: {
+    Authorization: process.env.CMS_API_AUTHORIZATION || ''
+  }
 });
 
 export default cmsClient;
