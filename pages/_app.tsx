3237e9660acc7236545fa763844849e10c76f951
João Giovanni <joaogiovannivilela@gmail.com>
2022-08-18 18:57:22 -0300

    Add animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import ThemeProvider from '../contexts/themes/ThemeProvider'
 import Layout from '../components/Layout'
 import { NextPage } from 'next'
 import { ReactElement, ReactNode } from 'react'
+import { motion } from 'framer-motion'
 
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -13,11 +14,24 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
 
-function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+function MyApp({ Component, pageProps, router }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => <Layout role={pageProps.role || 'user'}>{page}</Layout>)
 
   return <ThemeProvider>
-    {getLayout(<Component {...pageProps} />)}
+    <motion.div
+      key={router.route}
+      initial="initial"
+      animate="animate"
+      variants={{
+        initial: {
+          opacity: 0,
+        },
+        animate: {
+          opacity: 1,
+        },
+      }}>
+      {getLayout(<Component {...pageProps} />)}
+    </motion.div>
   </ThemeProvider>
 }
 
