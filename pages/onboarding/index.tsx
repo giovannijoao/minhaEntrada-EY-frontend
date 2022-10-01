95e8bfcff50c000addc3bf18eeccbca3aa35b24f
João Giovanni <joaogiovannivilela@gmail.com>
2022-10-01 14:23:41 -0300

    Certificados e cursos

--- pages/onboarding/index.tsx
+++ pages/onboarding/index.tsx
@@ -11,7 +11,24 @@ const tabs = [
 ]
 
 export default function OnBoarding() {
-  const form = useForm({
+  const form = useForm<{
+    firstName: string;
+    lastName: string;
+    birthDate: string;
+    email: string;
+    phoneNumber: string;
+    password: string;
+    passwordConfirm: string;
+    education: IEducation[];
+    certifications: {
+      name: string;
+      organization: string;
+      expiresAt: string;
+      issueDate: string;
+      credentialId: string;
+      credentialUrl: string;
+    }[]
+  }>({
     defaultValues: {
       // 1. Dados pessoais
       firstName: "João",
@@ -23,13 +40,13 @@ export default function OnBoarding() {
       passwordConfirm: "teste",
       // 2. Dados escolares e profissionais
       education: [{
-        id: '1',
         school: "FIAP",
         degree: "Bacharelado",
         grade: "Sistemas de Informação",
         startDate: "2019-03-01",
         endDate: "2022-12-01",
-      }]
+      }],
+      certifications: []
     }
   });
   const [tabIndex, setTabIndex] = useState(0)
@@ -174,7 +191,13 @@ export default function OnBoarding() {
                 </Button>
               </TabPanel>
               <TabPanel display={"flex"} flexDirection="column" alignItems={"center"} gap={8}>
-                <EducationForm />
+                <Flex direction={{
+                  base: 'column',
+                  md: 'row'
+                }} gap={6}>
+                  <EducationForm />
+                  <CertificationForm />
+                </Flex>
                 <Button bg="yellow.brand" color="gray.brand" mx="auto" rightIcon={<ChevronRightIcon />} onClick={handleNext}>
                   Próxima Etapa
                 </Button>
@@ -220,16 +243,13 @@ const EducationForm = () => {
   const errors = formAdd.formState.errors;
 
   const handleAdd = useCallback((values: IEducation) => {
-    console.log(values);
     append(values)
     onCloseAddEducationModal()
-  }, [append, onCloseAddEducationModal])
+    formAdd.reset()
+  }, [append, formAdd, onCloseAddEducationModal])
 
   return <>
-    <Flex direction="column" w={{
-      base: 'full',
-      md: "4xl"
-    }} mx={'auto'} gap={4}>
+    <Flex direction="column" w="full" h="full" gap={4}>
       <Flex p={8} boxShadow="md" bg="blackAlpha.500" w="lg" direction="column" gap={4}>
         <Flex justifyContent={"space-between"} w="full">
           <Heading fontSize="2xl">Formação Academica</Heading>
@@ -308,6 +328,140 @@ const EducationForm = () => {
               </FormControl>
 
             </Flex>
+          </ModalBody>
+          <ModalFooter>
+            <Button bg="yellow.brand" color="gray.brand" type="submit">Adicionar</Button>
+          </ModalFooter>
+        </form>
+      </ModalContent>
+    </Modal>
+  </>
+}
+
+type ICertification = {
+  name: string;
+  organization: string;
+  issueDate: string;
+  expiresAt?: string;
+  credentialId?: string;
+  credentialUrl?: string;
+}
+
+const CertificationForm = () => {
+  const { isOpen: isOpenAddCertificationModal, onOpen: onOpenAddCertificationModal, onClose: onCloseAddCertificationModal } = useDisclosure()
+  const form = useFormContext<{
+    certifications: ICertification[]
+  }>()
+  const { fields, append, prepend, remove, swap, move, insert } = useFieldArray({
+    control: form.control,
+    name: "certifications", // unique name for your Field Array
+  });
+
+  const formAdd = useForm({
+    defaultValues: {
+      name: "",
+      organization: "",
+      issueDate: "",
+      expiresAt: undefined,
+      credentialId: undefined,
+      credentialUrl: undefined,
+    }
+  })
+  const errors = formAdd.formState.errors;
+
+  const handleAdd = useCallback((values: ICertification) => {
+    append(values)
+    onCloseAddCertificationModal()
+    formAdd.reset();
+  }, [append, formAdd, onCloseAddCertificationModal])
+
+  return <>
+    <Flex direction="column" w="full" h="full" alignSelf={"stretch"} gap={4}>
+      <Flex p={8} boxShadow="md" bg="blackAlpha.500" w="lg" direction="column" gap={4}>
+        <Flex justifyContent={"space-between"} w="full">
+          <Heading fontSize="2xl">Certificados e Cursos</Heading>
+          <Button size="sm" bg="yellow.brand" color="gray.brand" rightIcon={<AddIcon />} onClick={onOpenAddCertificationModal}>
+            Adicionar
+          </Button>
+        </Flex>
+        <Divider />
+        <Flex direction="column" gap={2}>
+          {
+            fields.map(cert => {
+              const issuedAt = format(new Date(cert.issueDate), 'MMM yyyy')
+              const expiresAt = cert.expiresAt && format(new Date(cert.expiresAt), 'MMM yyyy')
+              return <Flex key={cert.id} direction="column">
+                <Heading fontSize="lg">{cert.name}</Heading>
+                <Text fontWeight={"light"}>Expedido {issuedAt} {expiresAt && ` - Expiração ${expiresAt}`}</Text>
+              </Flex>
+            })
+          }
+        </Flex>
+      </Flex>
+    </Flex>
+    <Modal isOpen={isOpenAddCertificationModal} onClose={onCloseAddCertificationModal}>
+      <ModalOverlay />
+      <ModalContent bg="gray.800">
+        <ModalHeader>Adicionar Certificado</ModalHeader>
+        <ModalCloseButton />
+        <form onSubmit={formAdd.handleSubmit(handleAdd)}>
+          <ModalBody display="flex" flexDirection={"column"} gap={2}>
+            <FormControl
+              isInvalid={!formAdd.formState.dirtyFields.name && !!errors.name}
+            >
+              <FormLabel>Nome*</FormLabel>
+              <Input type='text' {...formAdd.register('name', { required: "Campo obrigatório" })} />
+              {errors.name && <FormErrorMessage>
+                {errors.name?.message}
+              </FormErrorMessage>}
+            </FormControl>
+            <FormControl
+              isInvalid={!formAdd.formState.dirtyFields.organization && !!errors.organization}
+            >
+              <FormLabel>Organização*</FormLabel>
+              <Input type='text' {...formAdd.register('organization', { required: "Campo obrigatório" })} />
+              {errors.organization && <FormErrorMessage>
+                {errors.organization?.message}
+              </FormErrorMessage>}
+            </FormControl>
+            <Flex gap={4}>
+              <FormControl
+                isInvalid={!formAdd.formState.dirtyFields.issueDate && !!errors.issueDate}
+              >
+                <FormLabel>Data de Expedição*</FormLabel>
+                <Input type='date' {...formAdd.register('issueDate', { required: "Campo obrigatório" })} />
+                {errors.issueDate && <FormErrorMessage>
+                  {errors.issueDate?.message}
+                </FormErrorMessage>}
+              </FormControl>
+              <FormControl
+                isInvalid={!formAdd.formState.dirtyFields.expiresAt && !!errors.expiresAt}
+              >
+                <FormLabel>Data de Expiração</FormLabel>
+                <Input type='date' {...formAdd.register('expiresAt')} />
+                {errors.expiresAt && <FormErrorMessage>
+                  {errors.expiresAt?.message}
+                </FormErrorMessage>}
+              </FormControl>
+            </Flex>
+            <FormControl
+              isInvalid={!formAdd.formState.dirtyFields.credentialId && !!errors.credentialId}
+            >
+              <FormLabel>ID credencial</FormLabel>
+              <Input type='text' {...formAdd.register('credentialId', { required: "Campo obrigatório" })} />
+              {errors.credentialId && <FormErrorMessage>
+                {errors.credentialId?.message}
+              </FormErrorMessage>}
+            </FormControl>
+            <FormControl
+              isInvalid={!formAdd.formState.dirtyFields.credentialUrl && !!errors.credentialUrl}
+            >
+              <FormLabel>URL Credencial</FormLabel>
+              <Input type='text' {...formAdd.register('credentialUrl', { required: "Campo obrigatório" })} />
+              {errors.credentialUrl && <FormErrorMessage>
+                {errors.credentialUrl?.message}
+              </FormErrorMessage>}
+            </FormControl>
           </ModalBody>
           <ModalFooter>
             <Button bg="yellow.brand" color="gray.brand" type="submit">Adicionar</Button>
