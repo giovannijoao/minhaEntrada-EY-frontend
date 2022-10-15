ff44a9c86e271c7fc8810613aa366384d6c2d86e
João Giovanni <joaogiovannivilela@gmail.com>
2022-10-15 11:23:51 -0300

    Added title

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,19 @@
 // components/layout.js
 
 import { Box, Flex } from "@chakra-ui/react";
+import Head from "next/head";
 import HeaderV2 from "./Header";
 
 export default function Layout({ children, role }: any) {
   return (
-    <Flex direction="column" h="100vh">
-      <HeaderV2 role={role} />
-      <Box as="main" flex={1}>{children}</Box>
-    </Flex>
+    <>
+      <Head>
+        <title>minhaEntrada EY</title>
+      </Head>
+      <Flex direction="column" h="100vh">
+        <HeaderV2 role={role} />
+        <Box as="main" flex={1}>{children}</Box>
+      </Flex>
+    </>
   )
 }
